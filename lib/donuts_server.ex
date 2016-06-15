ffd315c0e1c28b60806c28161c15343d078a927e
yamamori.akihiro <yamamori.akihiro@donuts.ne.jp>
2016-06-15 15:49:37 +0900

    Implemented UDP echo server

--- lib/donuts_server.ex
+++ lib/donuts_server.ex
@@ -1,5 +1,17 @@
+require Logger
 defmodule DonutsServer do
   def run do
     IO.puts "`mix run -e DonutsServer.run` will run this script "
+  end
+
+  def udp_server do
+    {:ok, socket} = Socket.UDP.open 40001
+    loop socket
+  end
+  def loop(socket) do
+    {data, client} = socket |> Socket.Datagram.recv!
+    Logger.info(data)
+    socket |> Socket.Datagram.send(data, client) |> Logger.info
+    loop socket
   end
 end
