7ce38ced3738caa82c7c298744d2d75928493884
yamamori.akihiro <yamamori.akihiro@donuts.ne.jp>
2016-06-23 10:21:07 +0900

    Added ex_doc to deps. Closes #11

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule DonutsServer.Mixfile do
       {:message_pack, "~> 0.2.0"},
       {:logger_file_backend, "~> 0.0.8"},
       {:dialyxir, "~> 0.3", only: [:dev]},
+      {:ex_doc, "~> 0.11", only: :dev},
     ]
   end
 end
